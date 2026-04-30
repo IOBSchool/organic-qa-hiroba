9e49ed649ed3f8f0615c52ddc1289901ff50166c
IOBSchool <organiclifeingermany@gmail.com>
2026-04-30 19:31:33 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.QA_CONFIG = {
   POST_URL: "https://script.google.com/macros/s/AKfycbyhfYqKBWDNqAHowCdDAOpjGRQLX8q3WL5hS4LnQ13hoWqwaknQKyYPTjRpy0E9nyZ3Yg/exec",
-  GET_URL: "https://script.google.com/macros/s/AKfycbxz_iUuT6ENiLDJ6NTmSEwE2R_Rd14hOmjCHRtp9rAmaPpVEjmZ219C5nsJR41qehAs/exec",
+  GET_URL: "https://script.google.com/macros/s/AKfycbyhfYqKBWDNqAHowCdDAOpjGRQLX8q3WL5hS4LnQ13hoWqwaknQKyYPTjRpy0E9nyZ3Yg/exec",
   PASSWORD: "biohiroba"
 };
